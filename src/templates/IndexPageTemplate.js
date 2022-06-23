92d07c788c37d8357b07e9fab0f15e0959b51dd5
Adam <31741286+apmcguigan@users.noreply.github.com>
2022-06-23 00:50:28 -0300

    add pitch section to index page

--- src/templates/IndexPageTemplate.js
+++ src/templates/IndexPageTemplate.js
@@ -4,11 +4,12 @@ import { SectionHeading, TextImageSplit } from "../components/Sections"
 import { PrimaryButton, SecondaryButton } from "../components/Buttons"
 import { Container, CardSet } from "../components/Sections"
 import Image from "../components/Image"
+import { StaticImage } from "gatsby-plugin-image"
 
 const IndexPageTemplate = ({
   heading,
   subheading,
-  subhead1, 
+  subhead1,
   subhead2,
   line1,
   line2,
@@ -23,10 +24,14 @@ const IndexPageTemplate = ({
         <div className="w-4/5 md:w-1/2 mx-auto">
           <h3 className="font-bold text-beige text-3xl sm:pt-28">{line1}</h3>
           <h3 className="font-bold text-beige text-3xl py-2">{line2}</h3>
-          <h1 className="mt-2 text-beige text-3xl md:text-6xl md:py-2">{subhead1}</h1>
-          <h1 className="mt-2 text-beige text-3xl md:text-6xl md:py-2 pb-6">{subhead2}</h1>
+          <h1 className="mt-2 text-beige text-3xl md:text-6xl md:py-2">
+            {subhead1}
+          </h1>
+          <h1 className="mt-2 text-beige text-3xl md:text-6xl md:py-2 pb-6">
+            {subhead2}
+          </h1>
           <br></br>
-            <div className="w-1/3 md:w-1/5 h-8px bg-purple mx-auto"></div>
+          <div className="w-1/3 md:w-1/5 h-8px bg-purple mx-auto"></div>
 
           <PrimaryButton to="/contact" className="mt-6 lg:mt-10 self-center">
             WORK WITH ME
@@ -34,20 +39,45 @@ const IndexPageTemplate = ({
         </div>
       </div>
 
-      {/* Featured projects */}
-      <Container>
-        <div className="text-center">
-          <h2 className="text-2xl font-medium text-gray-800 sm:text-3xl lg:text-4xl">
-            I’m Nina. I help{" "}
-            <em>heart-centered entrepreneurs in health and wellness</em> with
-            the backend of their business so they can focus on what's most
+      {/*Pitch Section */}
+      <div className="relative min-w-full h-full overflow-hidden bg-beige text-center pb-20 pt-20 z-10 sm:pt-16 sm:pb-14 md:pt-16 md:pb-16 lg:pb-24 items-center">
+        <div className="w-4/5 md:w-3/4 mx-auto text-darkGreen">
+          <div className="text-2xl font-medium sm:text-3xl">
+            I help heart-centered entrepreneurs in health and wellness with the
+            backend of their business so they can focus on what's most
             important:
-          </h2>
-          <h1 className="text-3xl font-medium text-gray-900 sm:text-4xl lg:text-5xl ">
-            Test H1 text
-          </h1>
+          </div>
+            <br></br>
+          <div className="text-4xl font-medium mx-12">
+            CLIENT SERVICING, SUSTAINED GROWTH AND THE REASON THEY DO WHAT THEY
+            DO
+          </div>
+            <br></br>
+          <div className="w-1/3 md:w-1/5 h-8px bg-purple mx-auto"></div>
+            <br></br>
+          <div className="text-left text-5xl font-bold">LET ME GUESS...</div>
+          <br></br>
+          <ul className="text-xl text-left list-disc">
+            <li>You've always been able to wear all the hats in your business, but lately, you can't seem to shake the feeling that you're falling behind.</li>
+            <br></br>
+            <li>Your creative web tasks are distracting you from your main base and you want to get back to your passion - there just aren't enough hours in the day.</li>
+            <br></br>
+            <li>You have a clear vision, game-changing ideas, and goals to grow your business, but neither social media nor technology are where you want to spend your time, and you're beginning to feel stuck.</li>
+            <br></br>
+            <li>You want to be able to CONNECT with your base consistently on Social Media but you're not sure where to start...</li>
+          </ul>
+          <br></br>
+          <p className="text-4xl text-left">
+            YOU DON'T HAVE TO DO IT <b>ALONE</b>
+          </p>
+          <br></br>
+          <div className="w-1/3 md:w-1/5 h-8px bg-purple"></div>
+          <br></br>
+          <PrimaryButton to="/contact" className="mt-6 float-left">
+            GET STARTED
+          </PrimaryButton>
         </div>
-      </Container>
+      </div>
 
       {/* About section */}
       <TextImageSplit image={about.image}>
